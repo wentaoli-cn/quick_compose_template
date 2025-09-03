wentaoli-cn/compose_template

chore: initialize the project

app/src/main/java/com/iloatnew/template/MainActivity.kt
@@ -1,0 +1,30 @@
+package com.iloatnew.template
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.iloatnew.template.ui.res.AppTheme
+import com.iloatnew.template.ui.screen.AppNavHost
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
+
+        enableEdgeToEdge()
+
+        setContent {
+            AppTheme {
+                AppNavHost()
+            }
+        }
+    }
+}

app/src/main/java/com/iloatnew/template/ui/res/Color.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.template.ui.res
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val white = Color(0xFFFFFFFF)
+    val black = Color(0xFF000000)
+}

app/src/main/java/com/iloatnew/template/ui/screen/SplashScreen.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.template.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    // TODO
+}

wentaoli-cn/quick_compose_template

app/src/main/java/com/iloatnew/qct/MainActivity.kt
@@ -1,0 +1,27 @@
+package com.iloatnew.qct
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.iloatnew.qct.ui.res.AppTheme
+import com.iloatnew.qct.ui.res.AppNavHost
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.getInsetsController(window, window.decorView).apply {
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            hide(WindowInsetsCompat.Type.systemBars())
+        }
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                AppNavHost()
+            }
+        }
+    }
+}

app/src/main/java/com/iloatnew/qct/ui/res/Color.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.qct.ui.res
+
+import androidx.compose.ui.graphics.Color
+
+object AppColors {
+    val White = Color(0xFFFFFFFF)
+    val Black = Color(0xFF000000)
+}

app/src/main/java/com/iloatnew/qct/ui/screen/SplashScreen.kt
@@ -1,0 +1,8 @@
+package com.iloatnew.qct.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SplashScreen() {
+    // TODO
+}